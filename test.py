171140abc05ec5d3039a818fab2e219bf857968e
LendupLepcha <lenduplepcha62@gmail.com>
2021-03-23 13:41:40 +0530

    create package

---
+++ test.py
@@ -0,0 +1,11 @@
+from magnetics import magnetics as mg
+year = 2011
+month = 9
+day = 18
+hour = 12
+minute = 40
+latitude = 27.3314
+longitude = 88.6138
+
+
+mg.get_magnetic_data(latitude, longitude, year, month, day, hour, minute)
